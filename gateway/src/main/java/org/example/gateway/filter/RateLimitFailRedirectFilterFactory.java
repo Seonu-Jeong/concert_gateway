037533e7e699f97b6e5ce7af9dd94d6e32de60e8
Seonu-Jeong <sw.jeong97@gmail.com>
2025-07-07 16:11:56 +0900

    feature: 대기열 필터 팩토리 추가

---
+++ gateway/src/main/java/org/example/gateway/filter/RateLimitFailRedirectFilterFactory.java
@@ -0,0 +1,45 @@
+package org.example.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class RateLimitFailRedirectFilterFactory extends AbstractGatewayFilterFactory<RateLimitFailRedirectFilterFactory.Config> {
+
+	public RateLimitFailRedirectFilterFactory() {
+		super(Config.class);
+	}
+
+	@Override
+	public GatewayFilter apply(Config config) {
+		return (exchange, chain) ->
+			chain.filter(exchange).then(Mono.defer(() -> {
+				ServerHttpResponse response = exchange.getResponse();
+				HttpStatus status = (HttpStatus)response.getStatusCode();
+
+				if (status != null && status.value() == 429) {
+					response.setStatusCode(HttpStatus.SEE_OTHER); // 303 redirect
+					response.getHeaders().set("Location", config.getRedirectUrl());
+				}
+				return Mono.empty();
+			}));
+	}
+
+	public static class Config {
+		private String redirectUrl;
+
+		public String getRedirectUrl() {
+			return redirectUrl;
+		}
+
+		public void setRedirectUrl(String redirectUrl) {
+			this.redirectUrl = redirectUrl;
+		}
+	}
+}
